6dc173bd8b0011f388af53a53cd026cbaea84e03
Vlad Zusko <vladzvu11@gmail.com>
2023-10-27 00:42:40 +0300

    feat: update settings dividing

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,7 +1,8 @@
 export default class Settings {
   public static readonly DIVIDER = '|';
-  public static readonly PRECISION = 2;
 
-  public static readonly T_NEXT_PRECISION = 2;
-  public static readonly T_NEXT_PADDING = 20;
+  public static readonly TIME_PRECISION = 2;
+  public static readonly TIME_PADDING = 20;
+
+  public static readonly FLOAT_PRECISION = 2;
 }
