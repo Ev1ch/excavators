aad207c325534b733bb50b849231be489f368d12
Vlad Zusko <vladzvu11@gmail.com>
2023-10-26 23:54:15 +0300

    feat: add basic settings

---
+++ src/Settings.ts
@@ -0,0 +1,7 @@
+export default class Settings {
+  public static readonly DIVIDER = '|';
+  public static readonly PRECISION = 2;
+
+  public static readonly T_NEXT_PRECISION = 2;
+  public static readonly T_NEXT_PADDING = 20;
+}
