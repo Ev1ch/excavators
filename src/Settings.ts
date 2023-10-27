efdce73349a87c0871dd190052f4cc704126735c
Vlad Zusko <vladzvu11@gmail.com>
2023-10-28 00:26:47 +0300

    feat: update settings

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,8 +1,8 @@
 export default class Settings {
-  public static readonly DIVIDER = '|';
+  public static readonly DIVIDER = ' | ';
+  public static readonly PADDING = 30;
 
-  public static readonly TIME_PRECISION = 2;
-  public static readonly TIME_PADDING = 20;
+  public static readonly TIME_PRECISION = 4;
 
-  public static readonly FLOAT_PRECISION = 2;
+  public static readonly FLOAT_PRECISION = 5;
 }
